fbbbc33967c21043e4db51d922f206957728c074
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-07-29 17:31:20 -0500

    rolling dice game, basic java with Random util

---
+++ RollingDice.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class RollingDice {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        int x = rand.nextInt(6);
+        
+        System.out.println("You rolled a:" + x);
+    }
+}
