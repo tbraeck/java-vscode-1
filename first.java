e2bc9b859b07996c3bc63fbe7c2b41170430ca5d
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-07-29 17:23:15 -0500

    messingaround with the first app

--- first.java
+++ first.java
@@ -1,19 +1,30 @@
 /**
- * first
+ * First
  */
-public class first {
+public class First {
     public static void main(String[] args) {
     System.out.println("Fun times!");
     System.out.println("\nSecond fun string to print");
 
     System.out.println("\nSummery fun fantastic fun times!");
 
-    // int a = 5;
+        int[] a = new int[5];
+        char[] b = new char[5];
 
-    // for(i=0;i<5;i++) {
-    //     a(i)=i;
-    // }
-    // System.out.println("Frammy dammy" + i);
+        for(int i=0;i<5;i++) {
+            a[i]=i;
+        }
+
+        for(char i=0;i<5;i++) {
+            b[i]=i;
+        }
+   
+        for (int i = 0; i < 5; i++) {
+            System.out.println("I have" + ' ' + a[i] + ' ' + "problems, and" + ' ' + a[i] + ' ' + "ways to eat chicken tenders!");
+        }
+
+        System.out.println("Frammy dammy");
+
 }
 
 }
