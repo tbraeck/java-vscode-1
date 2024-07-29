19dc5fea63c2428213d01965655c91b3e49ef2e2
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-07-29 17:16:07 -0500

    changes to strings

--- first.java
+++ first.java
@@ -3,10 +3,10 @@
  */
 public class first {
     public static void main(String[] args) {
-    System.out.println("Fart");
-    System.out.println("\nFart arse");
+    System.out.println("Fun times!");
+    System.out.println("\nSecond fun string to print");
 
-    System.out.println("\nFart licker");
+    System.out.println("\nSummery fun fantastic fun times!");
 
     // int a = 5;
 
