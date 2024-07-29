74abcd69828e1ac8b07ab36ae63deede60ce8558
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-07-29 17:15:11 -0500

    first java project test

---
+++ first.java
@@ -0,0 +1,19 @@
+/**
+ * first
+ */
+public class first {
+    public static void main(String[] args) {
+    System.out.println("Fart");
+    System.out.println("\nFart arse");
+
+    System.out.println("\nFart licker");
+
+    // int a = 5;
+
+    // for(i=0;i<5;i++) {
+    //     a(i)=i;
+    // }
+    // System.out.println("Frammy dammy" + i);
+}
+
+}
