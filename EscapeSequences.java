7d7e46a08c75e2a6c437d306fd8858029f0f6c21
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-07-30 09:30:25 -0500

    escape commands

---
+++ EscapeSequences.java
@@ -0,0 +1,12 @@
+public class EscapeSequences {
+    public static void main(String[] args) {
+        System.out.println("ABC 123");
+        System.out.println("!@#$ \"fish\" ");
+        System.out.println("These are double quotes \"\" ");
+        System.out.println("These are double quotes \"\" ");
+        System.out.println("This is a backslash \\ ");
+        System.out.println("What do you mean \"stinky\"  ");
+        System.out.println("This is a tab \t!  ");
+//    escape characters- backslash for quotes and special character
+    }
+}
