363725603fd1b7b696d0b2d7f585fbb4191f3899
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-08-01 11:04:53 -0500

    more beginner stuff

---
+++ Cat.java
@@ -0,0 +1,15 @@
+    public class Cat {
+        
+        String name;
+        int age;
+
+        public void meow(){
+            System.out.println("Meow!");
+        }
+
+        public static void dingDong() {
+
+            System.out.println("Ding Dong Mr. Meow Meow!");
+        }
+    }
+
--- Pineapple.java
+++ Pineapple.java
@@ -1,5 +1,53 @@
+
+
+
+
+
+
 public class Pineapple {
     public static void main(String[] args) {
-        
+        // primitive types of variables name, type, value
+        // int myInt = 7;
+        // double shoeSize = 11.5;
+        // char myInitial = 'T';
+
+// non-primitive string
+
+Cat myCat = new Cat();
+myCat.name = "Tate";
+myCat.age = 12;
+myCat.meow();
+
+Cat anotherCat = new Cat();
+    //     String myName = " Tate";
+    //     myName.length();
+    //  printName("Tate", 3);
+
+    //     System.out.println(myInt*shoeSize);
+    //     System.out.println(myName.length());
+    for (int i = 0; i < 10; i++) {
+        System.out.println("These are awesome pretzels!");
+       Cat.dingDong();
+
     }
+
+    }
+
+    private static void printName(String name, int number) {
+        // System.out.println("My name is" + name);
+
+        if(name.equals("Tate")){
+            System.out.println("This guy is awesome!");
+        } else if (name.equals("Larry")) {
+            System.out.println("This guy is OK I guess!");
+        }else {
+            System.out.println("I don;t know this guy at all!");
+    
+        }
+    }
+
+    // conditional statements
+
+    
 }
+
